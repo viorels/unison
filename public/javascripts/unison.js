74425e9a0f6c51390f111f458ada5d8745617fe0
Viorel Stirbu <viorels@gmail.com>
2012-08-21 23:50:52 +0300

    implemented simple global chat

--- public/javascripts/unison.js
+++ public/javascripts/unison.js
@@ -1,7 +1,10 @@
 
 var searchForm = $('form#search_form')
-var searchResultsDiv = $('div.search_results')
-var otherSearches = $('div.other_searches ul')
+  , searchResultsDiv = $('div.search_results')
+  , otherSearches = $('div.other_searches ul')
+  , chatDiv = $('div.chat')
+  , chatForm = chatDiv.find('form')
+  , chatMessages = chatDiv.find('div.messages');
 
 var socket = io.connect('http://localhost');
 
@@ -10,18 +13,22 @@ $(function() {
 });
 
 function initSearch() {
-    $('form#search_form').submit(searchSubmit)
+    searchForm.submit(searchSubmit);
+}
+
+function initChat(keywords) {
+    chatDiv.show();
+    chatForm.submit(chatSubmit);
 }
 
 function searchSubmit() {
-    var keywords = $('form#search_form').find('input[name="keywords"]').val()
+    var keywords = searchForm.find('input[name="keywords"]').val()
     search(keywords);
     socket.emit('search', { keywords: keywords });
     return false;
 }
 
 function search(keywords) {
-    // var search_engine = 'http://www.google.com/search?q=' // no content returned
     var search_engine = 'https://duckduckgo.com/?q='
     searchResultsDiv.empty();
     $('<iframe />', {
@@ -31,7 +38,30 @@ function search(keywords) {
 }
 
 function searchByOther(keywords) {
-    otherSearches.append('<li>' + keywords + '</li>')
+    otherSearches.append('<li>' + keywords + '</li>');
+}
+
+function chatSubmit() {
+    var message_input = chatForm.find('input[name="message"]')
+    message = message_input.val();
+    if (message) {
+        message_input.val('');
+        chatSend(message);
+    };
+    return false;
+}
+
+function chatSend(message) {
+    chatMessage('me', message);
+    socket.emit('message', { nickname: '', message: message});
+}
+
+function chatReceive(data) {
+    chatMessage(data.nickname, data.message);
+}
+
+function chatMessage(nickname, message) {
+    chatMessages.append('<p>' + nickname + ': ' + message + '</p>');
 }
 
 socket.on('other_search', function (data) {
@@ -39,8 +69,14 @@ socket.on('other_search', function (data) {
     searchByOther(keywords);
 });
 
-socket.on('other_searches', function (keywords_list) {
-    keywords_list.forEach(function (keywords) {
+socket.on('other_searches', function (searches) {
+    searches.forEach(function (keywords) {
         searchByOther(keywords);
     })
 });
+
+socket.on('unison', function (data) {
+    initChat();
+})
+
+socket.on('message', chatReceive);
--- routes/index.js
+++ routes/index.js
@@ -18,13 +18,22 @@ var searches = []
 
 exports.connection = function (socket) {
   socket.on('search', search);
+  socket.on('message', message);
 
-  socket.emit('other_searches', searches); // not handled yet on the client
+  socket.emit('other_searches', searches);
 
   function search(data) {
     console.log(data);
     searches.push(data.keywords)
     socket.emit('other_search', { keywords: data.keywords });
     socket.broadcast.emit('other_search', { keywords: data.keywords });
+
+    socket.emit('unison', {});
+  }
+
+  function message(data) {
+    console.log(data);
+    nickname = data.nickname || 'anonymous';
+    socket.broadcast.emit('message', { nickname: nickname, message: data.message });
   }
 }
